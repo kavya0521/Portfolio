419326dad6d91dc2a94d787f0161fd9304880ce2
kavya0521 <pkavya.0521@gmail.com>
2026-06-11 16:13:09 +0530

    Delete Program 1 directory

--- Program 1/Basic-6.py
+++
@@ -1,4 +0,0 @@
-l=float(input("Enter the length:"))
-b=float(input("Enter the breadth:"))
-area=l*b
-print("Area of a rectangle is:%2f"%area)
--- Program 1/Basic-8.py
+++
@@ -1,7 +0,0 @@
-A=int(input("Please Enter first number A:"))
-B=int(input("Please Enter second number B:"))
-A=A*B
-B=A//B
-A=A//B
-print("The value of A after swapping:",A)
-print("The value of B after swapping:",B)
